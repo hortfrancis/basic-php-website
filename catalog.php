34ab382fe71e379e8536423d9dd777c2fcd24db6
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-10-19 17:18:38 +0100

    Update media display per category
    - Update media to display per each category via the links in the header navigation
    - Add functions to handle creating HTML markup
    - Add data for 12 media items as a multi-dimensional associative array

--- catalog.php
+++ catalog.php
@@ -1,5 +1,8 @@
 <?php
 
+include('inc/data.php');
+include('inc/functions.php');
+
 $pageTitle = "Full Catalog";
 $section = null;
 
@@ -18,9 +21,22 @@ if (isset($_GET['cat'])) {
 
 include('inc/header.php'); ?>
 
-<div class="section page">
+<div class="section catalog page">
 
-    <h1><?php echo $pageTitle; ?></h1>
+    <div class="wrapper">
+
+        <h1><?php echo $pageTitle; ?></h1>
+
+        <ul class='items'>
+            <?php
+            $categories = array_category($catalog, $section);
+            foreach ($categories as $id) {
+                echo get_item_html($id, $catalog[$id]); 
+            }
+            ?>
+        </ul>
+
+    </div>
 
 </div>
 
