76345291d4d40a013be6b05e01605f91a469ceb2
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-10-19 12:10:04 +0100

    Set page title based on category query
    - Set title attribute and <h1> based on the value of cat in the query string.

--- catalog.php
+++ catalog.php
@@ -1,12 +1,22 @@
-<?php 
+<?php
 
 $pageTitle = "Full Catalog";
+
+if (isset($_GET['cat'])) {
+    if ($_GET['cat'] === 'books') {
+        $pageTitle = 'Books';
+    } elseif ($_GET['cat'] === 'movies') {
+        $pageTitle = 'Movies';
+    } elseif ($_GET['cat'] === 'music') {
+        $pageTitle = 'Music';
+    }
+}
 
 include('inc/header.php'); ?>
 
 <div class="section page">
 
-    <h1>Full Catalog</h1>
+    <h1><?php echo $pageTitle; ?></h1>
 
 </div>
 
